a5808f8c008a8b9fbe831c571d5214548c64f130
BulliedDog <albertorovai1@gmail.com>
2024-07-08 16:15:13 +0200

    aggiunta versione 11

---
+++ C++/stl/character.h
@@ -0,0 +1,13 @@
+#include<iostream>
+template<typename T>
+class Character{
+    private:
+        T name {"cj"};
+        int age {18};
+    public:
+        Character(){}
+        Character(T name="Unknown", int age=18):name(name),age(age){}
+        ~Character(){} //no pointers so no dynamic memory, all static memory gets cleaned
+        T get_name(){return name;}
+        int get_age(){return age;}
+};
